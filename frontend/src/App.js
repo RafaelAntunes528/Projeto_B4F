e7750e488027610788d11d256af94a1d9ef170ba
RafaelAntunes528 <rafa13ant@gmail.com>
2022-09-27 14:14:07 +0100

    Users Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
-import Users_Board from './components/users';
+import UsersBoard from './components/users';
 
 function App() {
   return (
-    <div className="App">
-      <Users_Board></Users_Board>
+    <>
+    <div className='Mae'>
+    <div className='A'>
+      <input className='input'></input>
+      <select className='select'></select>
     </div>
+    <div className='P'>
+      <div className="App">
+        <UsersBoard nome="Rafa Gostoso" idade="19 anos" nif="123456789" status="Aprovado"/>
+      </div>
+      <div className="App">
+        <UsersBoard nome="Ginger" idade="18 anos" nif="123456789" status="Reprovado"/>
+      </div>
+    </div>
+    <div className='P'>
+      <div className="App">
+        <UsersBoard nome="Maria" idade="21 anos" nif="123456789" status="Reprovado"/>
+      </div>
+      <div className="App">
+        <UsersBoard nome="Babs" idade="22 anos" nif="123456789" status="Aprovado"/>
+      </div>
+    </div>
+    </div>
+    </>
+    
+    
   );
 }
 
