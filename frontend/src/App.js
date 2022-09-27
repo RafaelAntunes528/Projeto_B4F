40cd60e2dd04034db52d807569108d692fe9ce33
RafaelAntunes528 <rafa13ant@gmail.com>
2022-09-27 11:16:03 +0100

    HelloO

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Users_Board from './components/users';
+
+function App() {
+  return (
+    <div className="App">
+      <Users_Board></Users_Board>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/users.js
@@ -0,0 +1,14 @@
+import "./useres.css"
+export default function Users_Board(){
+    return(
+        <>
+        <div className={"Perfil"}>
+            <p>Nome: Rocky Rhodes</p>
+            <p>Idade: 19 anos</p>
+            <p>NIF: 123456789</p>
+            <p>Status: Aprovado</p>
+        </div>
+
+        </>
+    )
+}
